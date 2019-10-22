02bcca2186eab27ffea5c42c25465c0615c71739
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-10-22 14:01:59 +0300

    change pagination

--- index.php
+++ index.php
@@ -1,42 +1,10 @@
 <?php
-
+  
     include 'functions.php';
     $dir = 'img/small/';
     $bdir = 'img/big/';
     $images = get_images($dir);
-
-//----------- пагинация -----------//
-    // фото на страницу
-    $perpage = 6;
-    // общее количество фото
-    $count_img = count($images);
-    // необходимое число страниц
-    $count_pages = ceil($count_img / $perpage);
-    // если число страниц равно 0
-    if(!$count_pages) 
-        $count_pages = 1;
-    // получаем номер страницы 
-    if( isset($_GET['page'])){
-        $page = (int)$_GET['page'];
-        if($page < 1) 
-            $page = 1;
-    }else{
-        $page = 1;
-    }
-    // если запрашиваемая страница больше максимума
-    if ($page > $count_pages)
-        $page = $count_pages;
-    // номер первой картинки на страницу
-    $start_pos = ($page - 1) * $perpage;
-    // номер последней картинки на страницу 
-    $end_pos = $start_pos + $perpage;
-    // если номер последней картинки больше числа всех картинок
-    if($end_pos > $count_img)
-        $end_pos = $count_img;
-
-    // получаем пагинацию
-    $pagination = pagination($page, $count_pages)
-//--------------------//
+    require_once 'pagination.php';
 
 ?>
 
@@ -81,7 +49,7 @@
             <?php //endif; ?> -->
 
             <!-- ДЛЯ ПАГИНАЦИИ -->
-            <?php if($images): $i = $start_pos + 1?>
+            <?php if($images): $i = $start_pos + 1;?>
             <?php for($j = $start_pos; $j < $end_pos; $j++): ?>
                 <div class="item">
                     <div>
@@ -107,5 +75,6 @@
 
 <script src="http://code.jquery.com/jquery-latest.js"></script>
 <script src="js/lightbox.min.js"></script>
+<script src="js/script.js"></script>
 </body>
 </html>
---
+++ pagination.php
@@ -0,0 +1,32 @@
+<?php
+//----------- пагинация -----------//
+    // фото на страницу
+    $perpage = 3;
+    // общее количество фото
+    $count_img = count($images);
+    // необходимое число страниц
+    $count_pages = ceil($count_img / $perpage);
+    // если число страниц равно 0
+    if(!$count_pages) 
+        $count_pages = 1;
+    // получаем номер страницы 
+    if( isset($_GET['page'])){
+        $page = (int)$_GET['page'];
+        if($page < 1) 
+            $page = 1;
+    }else{
+        $page = 1;
+    }
+    // если запрашиваемая страница больше максимума
+    if ($page > $count_pages)
+        $page = $count_pages;
+    // номер первой картинки на страницу
+    $start_pos = ($page - 1) * $perpage;
+    // номер последней картинки на страницу 
+    $end_pos = $start_pos + $perpage;
+    // если номер последней картинки больше числа всех картинок
+    if($end_pos > $count_img)
+        $end_pos = $count_img;
+
+    // получаем пагинацию
+    $pagination = pagination($page, $count_pages);
