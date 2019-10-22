66d0a109819473c1c702b9b64098c0da7c78ab0b
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-10-22 11:55:33 +0300

    fix style

--- index.php
+++ index.php
@@ -5,6 +5,26 @@
     $bdir = 'img/big/';
     $images = get_images($dir);
 
+//----------- пагинация -----------//
+    // фото на страницу
+    $perpage = 6;
+    // общее количество фото
+    $count_img = count($images);
+    // необходимое число страниц
+    $count_pages = ceil($count_img / $perpage);
+    // если число страниц равно 0
+    if(!$count_pages) 
+        $count_pages = 1;
+    // получаем номер страницы 
+    if( isset($_GET['page'])){
+        $page = (int)$_GET['page'];
+        if($page < 1) 
+            $page = 1;
+    }else{
+        $page = 1;
+    }
+//---------- ----------//
+
 ?>
 
 <!doctype html>
@@ -19,6 +39,7 @@
 <body background="main_bg.jpg">
     <h1 align="center">Welcome to my Gallery!</h1>
 
+    <!-- БОКОВОЕ МЕНЮ -->
     <div class="category-wrap">
         <h3>Категории</h3>
         <ul>
@@ -31,7 +52,7 @@
     <div class="wrapper">
         <div class="gallery">
             <!-- ВЫВОД КАРТИНОК В ЦИКЛЕ -->
-            <?php if($images): $i = 1?>
+            <!-- <?php if($images): $i = 1?>
                 <?php foreach($images as $image): ?>
                     <div class="item">
                         <div>
@@ -44,8 +65,24 @@
                 <?php $i++; endforeach; ?>
             <?php else: ?>
                 <p>В данной галерее нет картинок</p>
-            <?php endif; ?>
+            <?php endif; ?> -->
 
+            <!-- ДЛЯ ПАГИНАЦИИ -->
+            <?php // echo "<pre>" .print_r($images, true). "</pre>" ?>
+            <?php if($images): $i = 1?>
+            <?php for($j = 0; $j < 6; $j++): ?>
+                <div class="item">
+                    <div>
+                        <a  data-lightbox="lightbox" href="<?= $bdir . $images[$j] ?>">
+                            <img class="front" src="<?= $dir . $images[$j] ?>" alt="">
+                            <span class="back">Фото <?=$i?></span>
+                        </a>
+                    </div>
+                </div>
+            <?php $i++; endfor; ?>
+        <?php else: ?>
+            <p>В данной галерее нет картинок</p>
+        <?php endif; ?>
         </div>
     </div>
 
