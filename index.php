a0eb3f8946bbb2318df696485f1cc833749df30b
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-10-22 10:35:04 +0300

    side menu w/ css

--- index.php
+++ index.php
@@ -12,15 +12,24 @@
 <head>
     <meta charset="UTF-8">
     <title>Gallery</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="css/lightbox.css">
 </head>
 
-<body>
+<body background="main_bg.jpg">
     <h1 align="center">Welcome to my Gallery!</h1>
+
+    <div class="category-wrap">
+        <h3>Категории</h3>
+        <ul>
+          <li><a href="">Портрет</a></li>
+          <li><a href="">Макро</a></li>
+          <li><a href="">Пейзаж</a></li>
+        </ul>
+    </div>
+
     <div class="wrapper">
         <div class="gallery">
-
             <!-- ВЫВОД КАРТИНОК В ЦИКЛЕ -->
             <?php if($images): $i = 1?>
                 <?php foreach($images as $image): ?>
@@ -38,7 +47,6 @@
             <?php endif; ?>
 
         </div>
-   
     </div>
 
 <script src="http://code.jquery.com/jquery-latest.js"></script>
