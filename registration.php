94223955558deb8419920138f199fec04a8c1809
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-10-28 19:13:29 +0300

    try to make reg and log

---
+++ registration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <form action="" class="form-singin" method="POST">
+                <h2>Registration</h2>
+                <input type="text" naem="username" class="form-control" placeholder="Username" required>
+                <input type="email" naem="email" class="form-control" placeholder="Email" required>
+                <input type="password" naem="password" class="form-control" placeholder="Passsword" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+            </form>
+        </div>
+    </body>
+</html>
