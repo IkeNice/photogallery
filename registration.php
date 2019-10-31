fd8961d3521cc3404dea3815796c9bb47cfdaf17
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-10-31 10:18:37 +0300

    change header style

--- registration.php
+++ registration.php
@@ -5,10 +5,12 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="css/loginstyle.css">
-        <link rel="stylesheet" type="text/css" href="<?=SITE?>css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     <!-- <link rel="stylesheet" href="<?=SITE?>css/lightbox.css"> -->
     </head>
+
 <body background="main_bg.jpg">
+
 <?php include "header.php"; ?>
 <?php
     require_once 'config.php';
